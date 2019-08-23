37c1a1cc84d33eead5b56e25523c07b23518ac02
MarcoMiretti <marcomiretti@gmail.com>
2019-08-23 20:15:46 +0200

    added the first skeleton of the project, it compiles but not sure if it would flash. The makefile should be taken with a grain of salt since it was lazy-generated with eclipsed, lacks of a proper linker and im not sure if the startup file is invoked properly

---
+++ inc/main.h
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    main.h
+  * @author  Marco Miretti
+  * @brief   TODO: write brief
+  *
+  *	TODO: write long project description
+  *
+  ******************************************************************************
+  * @section LICENSE TODO: choose license
+  ******************************************************************************
+  */
+
+/* Prevent recursive inclusion */
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f4xx.h"
+
+#endif /* __MAIN_H */
---
+++ src/main.c
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Marco Miretti
+  * @brief   TODO: write brief
+  *
+  *	TODO: write long project description
+  *
+  ******************************************************************************
+  * @section LICENSE TODO: choose license
+  ******************************************************************************
+  */
+
+#include "main.h"
+
+/**
+  * @brief  Application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+	/* Infinite Loop */
+	while(1)
+	{
+
+	}
+	return 0;
+}
