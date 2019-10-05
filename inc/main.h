1147b3af265843d89210594eea6ddbdd3f303472
MarcoMiretti <marcomiretti@gmail.com>
2019-10-05 12:22:17 -0300

    programación del timer 3 para decodificar el encoder, arreglé las tabulaciones

--- inc/main.h
+++ inc/main.h
@@ -21,38 +21,30 @@
 /** @defgroup GPIO_Constants
   * @{
   */
-#define LD4_GPIO_PIN 		(1 << 12)  									/* PD12 */
-#define LD4_MODE_OUT		(1 << 24)  									/* General Purpose Input for PD12 */
-#define LD5_GPIO_PIN 		(1 << 14)  									/* PD14 */
-#define PD14_AFRH_AF2		((uint32_t)0x02000000)						/* PD14 to TIM4 */
-#define PD14_OSPEEDR_VHS	((uint32_t)0x30000000)						/* PD14 High Speed (100Mhz and beyond) */
-#define LD5_MODE_ALT		((uint32_t)0x20000000)  					/* General Purpose Input for PD14 */
+#define LD4_GPIO_PIN 		(1 << 12)  				/* PD12 */
+#define LD4_MODE_OUT		(1 << 24)  				/* General Purpose Input for PD12 */
+#define LD5_GPIO_PIN 		(1 << 14)  				/* PD14 */
+#define PD14_AFRH_AF2		((uint32_t)0x02000000)			/* PD14 to TIM4 */
+#define PD14_OSPEEDR_VHS	((uint32_t)0x30000000)			/* PD14 High Speed (100Mhz and beyond) */
+#define LD5_MODE_ALT		((uint32_t)0x20000000)  		/* General Purpose Input for PD14 */
 /**
   * @}
   */
   
-/** @defgroup RCC_Constants
-  * @{
-  */
-#define GPIOD_EN  			(1 << 3)   									/* RCC GPIOD Enable Bit */
-#define TIM4_EN  			(1 << 2)   									/* RCC TIM4 Enable Bit */
-/**
-  * @}
-  */
 
 /** @defgroup TIM_Constants
   * @{
   */
-#define TIM4_ARR 								((uint16_t)0x20CF)		/* Auto reload register = 84000000 / 10000 - 1 = 8399 set for 10kHz PWM*/
-#define TIM_PSCReloadMode_Immediate        		((uint16_t)0x0001)
+#define TIM4_ARR 			((uint16_t)0x20CF)		/* Auto reload register = 84000000 / 10000 - 1 = 8399 set for 10kHz PWM*/
+#define TIM_PSCReloadMode_Immediate     ((uint16_t)0x0001)
 /** @defgroup PWM_Constants
   * @{
   */
-#define TIM4_CCMR2_OC3M 						((uint16_t)0x0070)		/* PWM mode 2 */
-#define TIM4_CCER_CC3E							((uint16_t)0x0001 << 8)
-#define TIM4_CCER_CC3P							((uint16_t)0x0002 << 8) /* Polarity Low */
-#define TIM4_CCR3								((uint16_t)0x00FF)  	/* 25% duty TODO: implement a macro */
-#define TIM4_OCPreload_Enable               	((uint16_t)0x0008)
+#define TIM4_CCMR2_OC3M 		((uint16_t)0x0070)		/* PWM mode 2 */
+#define TIM4_CCER_CC3E			((uint16_t)0x0001 << 8)
+#define TIM4_CCER_CC3P			((uint16_t)0x0002 << 8) 	/* Polarity Low */
+#define TIM4_CCR3			((uint16_t)0x00FF)  		/* 25% duty TODO: implement a macro */
+#define TIM4_OCPreload_Enable           ((uint16_t)0x0008)
 /**
   * @}
   */
--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@
 #include "main.h"
 
 uint8_t GPIO_Init(void);
+uint8_t TIM3_Init(void);
 uint8_t TIM4_Init(void);
 uint8_t PWM_Init(void);
 uint8_t PWM_SetDuty(uint8_t duty);
@@ -27,6 +28,7 @@ uint8_t blinkLed4(void);
 int main(void)
 {
 	GPIO_Init();
+	TIM3_Init();
 	TIM4_Init();
 	PWM_Init();
 	blinkLed4();
@@ -39,16 +41,39 @@ int main(void)
   */
 uint8_t GPIO_Init(void)
 {
-	/* Clock for GPIOD */
-	RCC->AHB1ENR |= GPIOD_EN;
+	/* Clock for GPIOD and GPIOC */
+	RCC->AHB1ENR 	|= (uint32_t)(RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOCEN);
 	
-	/* Set LED4 (PD12) as GPIOD output */
-	GPIOD->MODER |= LD4_MODE_OUT;
-	GPIOD->ODR	 |= LD4_GPIO_PIN;
+	/* PD12 (LED 4) */
+	GPIOD->MODER 	&= (uint32_t)(~GPIO_MODER_MODE12_1);					
+	GPIOD->MODER 	|= (uint32_t)GPIO_MODER_MODE12_0; 					/* Set LED4 (PD12) as GPIOD output */
+	GPIOD->ODR	 	|= (uint32_t)GPIO_ODR_OD12;
 	
-	GPIOD->MODER |= LD5_MODE_ALT;
-	GPIOD->OSPEEDR |= PD14_OSPEEDR_VHS;
-	GPIOD->AFR[1]	 |= PD14_AFRH_AF2;
+	/* PD14 (PWM - LED 5) */
+	GPIOD->MODER 	|= (uint32_t)GPIO_MODER_MODER14_1;
+	GPIOD->MODER 	&= (uint32_t)(~GPIO_MODER_MODER14_0);					/* PD14 to alternate function MODER14 = 0b10 */
+	
+	GPIOD->OSPEEDR 	|= (uint32_t)(GPIO_OSPEEDER_OSPEEDR14_1 | GPIO_OSPEEDER_OSPEEDR14_0); 	/* PD14 to very high speed mode */
+	
+	GPIOD->AFR[1] 	&= (uint32_t)~(GPIO_AFRH_AFRH6_3 | GPIO_AFRH_AFRH6_2 | GPIO_AFRH_AFRH6_0);
+	GPIOD->AFR[1] 	|= (uint32_t)(GPIO_AFRH_AFRH6_1);					/* PD14 mapped to AF2 (TIM4) */
+
+	/* PC6 & PC7 (quadrature encoder) */
+	GPIOC->MODER 	|= (uint32_t)GPIO_MODER_MODER6_1;
+	GPIOC->MODER 	&= (uint32_t)~(GPIO_MODER_MODER6_0);					/* PC6 to alternate function MODER6 = 0b10 */
+	GPIOC->MODER 	|= (uint32_t)GPIO_MODER_MODER7_1;
+	GPIOC->MODER 	&= (uint32_t)~(GPIO_MODER_MODER7_0); 					/* PC7 to alternate function MODER7 = 0b10 */
+	
+	//GPIOC->OSPEEDR 	|= (uint32_t)(GPIO_OSPEEDER_OSPEEDR6_1 | GPIO_OSPEEDER_OSPEEDR6_0); 	/* PC6 to very high speed mode */
+	//GPIOC->OSPEEDR 	|= (uint32_t)(GPIO_OSPEEDER_OSPEEDR7_1 | GPIO_OSPEEDER_OSPEEDR7_0); 	/* PC7 to very high speed mode */
+
+	GPIOC->PUPDR 	|= (uint32_t)GPIO_PUPDR_PUPDR6_0; 					/* PC6 to pull-up */
+	GPIOC->PUPDR 	|= (uint32_t)GPIO_PUPDR_PUPDR7_0; 					/* PC7 to pull-up */
+
+	GPIOC->AFR[0] 	&= (uint32_t)~(GPIO_AFRL_AFRL6_3 | GPIO_AFRL_AFRL6_2 | GPIO_AFRL_AFRL6_0);
+	GPIOC->AFR[0] 	|= (uint32_t)(GPIO_AFRL_AFRL6_1);					/* PC6 mapped to AF2 (TIM3 CH1) */
+	GPIOC->AFR[0] 	&= (uint32_t)~(GPIO_AFRL_AFRL7_3 | GPIO_AFRL_AFRL7_2 | GPIO_AFRL_AFRL7_0);
+	GPIOC->AFR[0] 	|= (uint32_t)(GPIO_AFRL_AFRL7_1);					/* PC7 mapped to AF2 (TIM3 CH2) */	
 	return 0;
 }
 
@@ -65,15 +90,15 @@ uint8_t TIM4_Init(void)
 	uint16_t tmpcr1 = 0;
 	tmpcr1 = TIM4->CR1;
 	
-    tmpcr1 &= (uint16_t)(~(TIM_CR1_DIR | TIM_CR1_CMS));					/* Set the Counter Mode (edge up) */
+	tmpcr1 &= (uint16_t)(~(TIM_CR1_DIR | TIM_CR1_CMS));				/* Set the Counter Mode (edge up) */
 	
 	TIM4->CR1 = tmpcr1;
 
-	TIM4->ARR  = TIM4_ARR; 												/* Sets timer period */
+	TIM4->ARR  = TIM4_ARR; 								/* Sets timer period */
 	TIM4->PSC = 0;
-	TIM4->EGR = TIM_PSCReloadMode_Immediate;  							/* Reload PSC */
+	TIM4->EGR = TIM_PSCReloadMode_Immediate;  					/* Reload PSC */
 	
-	TIM4->CR1 |= TIM_CR1_CEN;											/* Enable TIM4 */
+	TIM4->CR1 |= TIM_CR1_CEN;							/* Enable TIM4 */
 	return 0;
 }
 
@@ -83,44 +108,37 @@ uint8_t TIM4_Init(void)
   */
 uint8_t TIM3_Init(void)
 {
-	#define TIM3_EN  			(1 << 1) 
-	//#define TIM3_ARR 								((uint16_t)0x20CF)		/* Auto reload register = 84000000 / 10000 - 1 = 8399 set for 10kHz PWM*/
-	//#define TIM_PSCReloadMode_Immediate        		((uint16_t)0x0001)
-	
-	
 	/* Clock for TIM3*/
 	RCC->APB1ENR |= TIM3_EN;
 	
 	/* Set TIM3 */
-	uint16_t tmpcr1 = 0, tmpccmr1 = 0, tmpccmr2 = 0, tmpccer = 0, tmpsmcr = 0;
+	uint16_t tmpcr1 = 0, tmpccmr1 = 0, tmpccer = 0, tmpsmcr = 0;
+	
 	tmpcr1 = TIM3->CR1;
-	
-    tmpcr1 &= (uint16_t)(~(TIM_CR1_DIR | TIM_CR1_CMS));					/* Set the Counter Mode (edge up) */
-	
+    	tmpcr1 &= (uint16_t)(~(TIM_CR1_DIR | TIM_CR1_CMS | TIM_CR1_CEN));		/* Set the Counter Mode (edge up) */
 	TIM3->CR1 = tmpcr1;
 
-
 	tmpccmr1 = TIM3->CCMR1;
-	tmpccmr1 |= TIM_CCMR1_CC1S_0;											/* TI1FP1 mapped on TI1 */
-	//TIM3->CCMR1 = tmpccmr1;
-	
-	//tmpccmr1 = TIM3->CCMR2;
-	tmpccmr1 |= TIM_CCMR1_CC2S_0;											/* TI2FP2 mapped on TI2 */
+	tmpccmr1 &= (uint16_t)~(TIM_CCMR1_CC1S_1 | TIM_CCMR1_CC2S_1);
+	tmpccmr1 |= (uint16_t)(TIM_CCMR1_CC1S_0 | TIM_CCMR1_CC2S_0);			/* TI1FP1 mapped on TI1 & TI2FP2 mapped on TI2 */
+	tmpccmr1 |= (uint16_t)(TIM_CCMR1_IC1F | TIM_CCMR1_IC2F); 			/* Input filter for CH1 and CH2 */
 	TIM3->CCMR1 = tmpccmr1;
 	
 	tmpccer = TIM3->CCER;
-	tmpccer &= (uint16_t)(~(TIM_CCER_CC1P | TIM_CCER_CC1NP));			/* TI1FP1 noninverted, TI1FP1=TI1 */
-	tmpccer &= (uint16_t)(~(TIM_CCER_CC2P | TIM_CCER_CC2NP));			/* TI2FP2 noninverted, TI2FP2=TI2 */
+	tmpccer &= (uint16_t)(~(TIM_CCER_CC1P | TIM_CCER_CC1NP));			/* TI1FP1 noninverted */
+	tmpccer &= (uint16_t)(~(TIM_CCER_CC2P | TIM_CCER_CC2NP));			/* TI2FP2 noninverted */
+	TIM3->CCER = tmpccer;
 
 	tmpsmcr = TIM3->SMCR;
-	tmpsmcr |= (uint16_t)(~(TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1));
-	tmpsmcr &= (uint16_t)(~(TIM_SMCR_SMS_2));							/* both inputs are active on both rising and falling edges */
+	tmpsmcr |= (uint16_t)(TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1);
+	tmpsmcr &= (uint16_t)(~(TIM_SMCR_SMS_2));					/* both inputs are active on both rising and falling edges */
+	TIM3->SMCR = tmpsmcr;
 
-	//TIM4->ARR  = TIM3_ARR; 											/* Sets timer period */
-	//TIM4->PSC = 0;
-	//TIM4->EGR = TIM_PSCReloadMode_Immediate;  						/* Reload PSC */
+	TIM3->ARR  = 0xFFFF; 								/* Sets timer period */
+	TIM3->CNT  = 0;
+	TIM3->EGR  |= 1; 								/* Reload PSC */
 	
-	TIM4->CR1 |= TIM_CR1_CEN;											/* Enable TIM4 */
+	TIM3->CR1 |= TIM_CR1_CEN;							/* Enable TIM3 */
 	return 0;
 }
 
@@ -133,25 +151,25 @@ uint8_t PWM_Init(void)
 	/* Set PWM */
 	uint16_t tmpccmrx = 0, tmpccer = 0, tmpcr2 = 0;
 	
-	TIM4->CCER &= (uint16_t)~TIM_CCER_CC3E;								/* Disable the Channel 1: Reset the CC1E Bit */
+	TIM4->CCER &= (uint16_t)~TIM_CCER_CC3E;						/* Disable the Channel 1: Reset the CC1E Bit */
 	
-	tmpccer = TIM4->CCER;												/* Get the TIM4 CCER register value */
-	tmpcr2 =  TIM4->CR2;												/* Get the TIM4 CR2 register value */
-	tmpccmrx = TIM4->CCMR1;												/* Get the TIM4 CCMR1 register value */
+	tmpccer = TIM4->CCER;								/* Get the TIM4 CCER register value */
+	tmpcr2 =  TIM4->CR2;								/* Get the TIM4 CR2 register value */
+	tmpccmrx = TIM4->CCMR1;								/* Get the TIM4 CCMR1 register value */
 	
-	tmpccmrx &= (uint16_t)~TIM_CCMR2_OC3M;								/* Reset the Output Compare Mode Bits */
+	tmpccmrx &= (uint16_t)~TIM_CCMR2_OC3M;						/* Reset the Output Compare Mode Bits */
 	tmpccmrx &= (uint16_t)~TIM_CCMR2_CC3S; 
-	tmpccer &= (uint16_t)~TIM_CCER_CC3P;								/* Reset the Output Polarity level */
+	tmpccer &= (uint16_t)~TIM_CCER_CC3P;						/* Reset the Output Polarity level */
 	
-	tmpccmrx |= TIM4_CCMR2_OC3M;										/* Select the Output Compare Mode */
+	tmpccmrx |= TIM4_CCMR2_OC3M;							/* Select the Output Compare Mode */
   
-	tmpccer |= TIM4_CCER_CC3P;											/* Set the Output Compare Polarity */
-	tmpccer |= TIM4_CCER_CC3E;											/* Set the Output State */
+	tmpccer |= TIM4_CCER_CC3P;							/* Set the Output Compare Polarity */
+	tmpccer |= TIM4_CCER_CC3E;							/* Set the Output State */
 		  
-	TIM4->CR2 = tmpcr2;													/* Write to TIMx CR2 */
-	TIM4->CCMR2 = tmpccmrx;												/* Write to TIMx CCMR1 */
-	TIM4->CCR3	= TIM4_CCR3;											/* Set duty */
-	TIM4->CCER	= tmpccer;												/* Enable PWM2 output */
+	TIM4->CR2 = tmpcr2;								/* Write to TIMx CR2 */
+	TIM4->CCMR2 = tmpccmrx;								/* Write to TIMx CCMR1 */
+	TIM4->CCR3	= TIM4_CCR3;							/* Set duty */
+	TIM4->CCER	= tmpccer;							/* Enable PWM2 output */
 	TIM4->CCMR2 |= TIM4_OCPreload_Enable;
 	return 0;
 }
@@ -181,6 +199,7 @@ uint8_t blinkLed4(void)
 	while(1)
 	{
 		for (i = 0; i < 1000000; ++i) ; GPIOD -> ODR |= LD4_GPIO_PIN;
+		PWM_SetDuty((TIM3->CNT*100)/0xFFFF);
 		for (i = 0; i < 1000000; ++i) ; GPIOD -> ODR &= ~LD4_GPIO_PIN;
 	}
 	return 0;
