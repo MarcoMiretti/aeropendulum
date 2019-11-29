821c0d828550bf1385537015335721330ed0656a
MarcoMiretti <marco.miretti@gmail.com>
2019-11-29 14:21:41 -0300

    :sparkles: Replace arm pid with my own
    The arm_dsp pid implementation was good, but i preffered to try my own
    implementation for fun.

--- firmware/sources/src/driving.cpp
+++ firmware/sources/src/driving.cpp
@@ -103,6 +103,30 @@ class pid_controller
 {
 	public:
 		/**
+		 * \brief	Calculate a and b coeficients.
+		 * */
+		void	init(void);		
+		/**
+		 * \brief	Detects saturation
+		 * \note	If the motors are saturating (u0 is under or over limits) then u0 is set to that limit. Furthermore a saturating flag is risen, depending on the direction of the saturation. 
+		 */
+		float saturationDetector(void);
+		/**
+		 * \brief	Detects possible integrator windup and clamps or unclamps integration
+		 * \param	error = the error used to see the direction of integration
+		 */
+		void integratorClamping(float);
+		/**
+		 * \brief Update parameters according to gains
+		 */
+		void update_parameters(void);
+		/**
+		 * \brief 	Update manipulated variable according to PID params and previous states
+		 * \param	error = difference between set_point and actual value
+		 * \return	the manipulated variable
+		 */
+		float update_manipulated(float);
+		/**
 		 * \brief	Set PID Kd constant.
 		 * */
 		void	set_Kd(float);
@@ -115,9 +139,54 @@ class pid_controller
 		 * */
 		void	set_Kp(float);
 		/**
+		 * \brief	Set PID Kd command constant.
+		 * */
+		void	set_Kd_c(float);
+		/**
+		 * \brief	Set PID Ki command constant.
+		 * */
+		void	set_Ki_c(float);
+		/**
+		 * \brief	Set PID Kp command constant.
+		 * */
+		void	set_Kp_c(float);
+		/**
+		 * \brief	Set PID Kd reject constant.
+		 * */
+		void	set_Kd_r(float);
+		/**
+		 * \brief	Set PID Ki reject constant.
+		 * */
+		void	set_Ki_r(float);
+		/**
+		 * \brief	Set PID Kp reject constant.
+		 * */
+		void	set_Kp_r(float);
+		/**
 		 * \brief	Set PID Ts constant.
 		 * */
 		void	set_Ts(float);
+		/**
+		 * \brief	Set lowpass filter constant.
+		 * */
+		void	set_N(float);
+		/**
+		 * \brief	Set windup high limit.
+		 * */
+		void	set_wH(float);
+		/**
+		 * \brief	Set windup low limit.
+		 * */
+		void	set_wL(float);
+		/**
+		 * \brief	Set feedback linearization constant.
+		 * */
+		void	set_feedback_linearization(float);
+		/**
+		 * \brief	Set command mode vs reject mode threshold.
+		 * */
+		void	set_command_vs_reject_thres(float);
+		
 		
 		/**
 		 * \brief	Get PID kd constant.
@@ -135,15 +204,100 @@ class pid_controller
 		 * */
 		float 	get_Kp(void);
 		/**
-		 * \brief	Get PID kp constant.
+		 * \brief	Get PID kd command constant.
+		 * \retval 	kd constant
+		 * */
+		float 	get_Kd_c(void);
+		/**
+		 * \brief	Get PID ki command constant.
+		 * \retval 	ki constant
+		 * */
+		float 	get_Ki_c(void);
+		/**
+		 * \brief	Get PID kp command constant.
+		 * \retval 	kp constant
+		 * */
+		float 	get_Kp_c(void);
+		/**
+		 * \brief	Get PID kd reject constant.
+		 * \retval 	kd constant
+		 * */
+		float 	get_Kd_r(void);
+		/**
+		 * \brief	Get PID ki reject constant.
+		 * \retval 	ki constant
+		 * */
+		float 	get_Ki_r(void);
+		/**
+		 * \brief	Get PID kp reject constant.
+		 * \retval 	kp constant
+		 * */
+		float 	get_Kp_r(void);
+		/**
+		 * \brief	Get PID Ts constant.
 		 * \retval 	Ts constant
 		 * */
 		float 	get_Ts(void);
+		/**
+		 * \brief	Get lowpass filter constant.
+		 * \retval 	N constant
+		 * */
+		float 	get_N(void);
+		/**
+		 * \brief	Get windup high limit.
+		 * \retval 	wH constant
+		 * */
+		float 	get_wH(void);
+		/**
+		 * \brief	Get windup low limit.
+		 * \retval 	wL constant
+		 * */
+		float 	get_wL(void);
+		/**
+		 * \brief	Get feedback linearization.
+		 * \retval 	feedback linearization constant
+		 * */
+		float 	get_feedback_linearization(void);
+		/**
+		 * \brief	Get command mode vs reject mode threshold angle.
+		 * \retval 	feedback linearization constant
+		 * */
+		float 	get_command_vs_reject_thres(void);
 	private:
 		float	Kd;
 		float	Ki;
 		float	Kp;
-		float	Ts;
+	
+		float	Kd_c;
+		float	Ki_c;
+		float	Kp_c;
+
+		float	Kd_r;
+		float	Ki_r;
+		float	Kp_r;
+		
+		float	command_vs_reject_thres;
+
+		float	Ts;	/**< Sampling time */
+		float	N;	/**< Derivative term lowpass filter constant */
+		float	wH;	/**< Windup High limit */
+		float	wL;	/**< Windup Low limit */
+		
+		float	is_saturating_H;
+		float	is_saturating_L;
+		float	clamping_flag;
+		float	tmpKi;
+		float	feedback_linearization;
+
+		// Terms of the difference equation
+		float	a[3];
+		float	b[3];
+
+		// Controller output
+		float	u[3];
+
+		// Error signal
+		float	e[3];
 };
 /**
   * @brief  Change pwm duty value.
@@ -214,22 +368,45 @@ void aero_driving(void *pvParameters)
 	aeropendulum aero;
 	pid_controller pid;
 	/* aerodynamic constants */
-	float mk = 0.21970103*0.93;
-	float u0 = 2.12768394;
-	float ninetyConst = 0.08;
+	float mk = 0.15935738*0.88;//0.21970103;
+	float u0 = 2.11459009;//2.12768394;
+	float ninetyConst = 0.02;//0.02;
 	aero.set_propellerConst_MK(mk);
 	aero.set_propellerConst_u0(u0);
 	aero.set_over90_compensation_cohef(ninetyConst);
 
 	/* pid constants */
-	float sampling_time = 10;//milliseconds
-	float Kp = 0.03;
-	float Ki = 0.0001;
-	float Kd = 0.003;
-	pid.set_Kp(Kp);
-	pid.set_Ki(Ki);
-	pid.set_Kd(Kd);
+	//float Kp = 0.04;
+	//float Ki = 0.0001;
+	//float Kd = 0.002;
+	
+	float Kp_c = 0.045;
+	float Ki_c = 0.00002;
+	float Kd_c = 0.000005;
+
+	float Kp_r = 0.035;
+	float Ki_r = 0.000008;
+	float Kd_r = 0.000001;
+
+	float command_vs_reject_thres = 0.1745; //when is +- 10 grad from set point switch to reject mode
+
+	float sampling_time = 200;//milliseconds
+	float derivative_filter_cutoff = 1000;
+	float wH = 2.35;
+	float wL = 2.1;
+	pid.set_Kp_c(Kp_c);
+	pid.set_Ki_c(Ki_c);
+	pid.set_Kd_c(Kd_c);
+	//pid.set_Kp_r(Kp_r);
+	//pid.set_Ki_r(Ki_r);
+	//pid.set_Kd_r(Kd_r);
+
+	//pid.set_command_vs_reject_thres(command_vs_reject_thres);
+
 	pid.set_Ts(sampling_time);
+	pid.set_N(derivative_filter_cutoff);
+	pid.set_wH(wH);
+	pid.set_wL(wL);
 
 	/* set point */
 	float set_point = PI/2;
@@ -358,6 +535,95 @@ uint8_t PWM_setDuty_milli(float duty)
  * \addtogroup pid_member_functions PID members
  * @{
  */
+
+void pid_controller::init()
+{
+	pid_controller::update_parameters();
+	pid_controller::e[0] = 0;
+	pid_controller::e[1] = 0;
+	pid_controller::e[2] = 0;
+	pid_controller::u[0] = 0;
+	pid_controller::u[1] = 0;
+	pid_controller::u[2] = 0;
+}
+
+void pid_controller::update_parameters()
+{
+	pid_controller::a[0] = (1+pid_controller::N*pid_controller::Ts);
+	pid_controller::a[1] = -(2+pid_controller::N*pid_controller::Ts);
+	pid_controller::a[2] = 1;
+
+	pid_controller::b[0] = pid_controller::Kp*pid_controller::a[0] + pid_controller::Ki*pid_controller::Ts*pid_controller::a[0] + pid_controller::Kd*pid_controller::N;
+	pid_controller::b[1] = pid_controller::Kp*pid_controller::a[1] - pid_controller::Ki*pid_controller::Ts - 2*pid_controller::Kd*pid_controller::Ts;
+	pid_controller::b[2] = pid_controller::Kp + pid_controller::Kd*pid_controller::N;
+}
+
+float pid_controller::update_manipulated(float error)
+{
+	pid_controller::integratorClamping(error);
+	pid_controller::e[2] = pid_controller::e[1];
+	pid_controller::e[1] = pid_controller::e[0];
+	pid_controller::e[0] = error;
+
+	pid_controller::u[2] = pid_controller::u[1];
+	pid_controller::u[1] = pid_controller::u[0];
+
+	pid_controller::u[0] =(-pid_controller::a[1]*pid_controller::u[1] + \
+			       -pid_controller::a[2]*pid_controller::u[2] + \
+			        pid_controller::b[0]*pid_controller::e[0] + \
+			        pid_controller::b[1]*pid_controller::e[1] + \
+			        pid_controller::b[2]*pid_controller::e[2])/pid_controller::a[0]; 
+
+	return pid_controller::saturationDetector();
+}
+
+float pid_controller::saturationDetector()
+{
+	/* motor saturation detection */
+	if(pid_controller::u[0]+pid_controller::feedback_linearization > pid_controller::wH)
+	{
+		pid_controller::is_saturating_H = 1;
+		pid_controller::u[0] = pid_controller::wH-pid_controller::feedback_linearization;
+	}
+	else
+	{
+		pid_controller::is_saturating_H = 0;
+	}
+	if(pid_controller::u[0]+pid_controller::feedback_linearization < pid_controller::wL)
+	{
+		pid_controller::is_saturating_L = 1;
+		pid_controller::u[0] = pid_controller::wL-pid_controller::feedback_linearization;
+	}
+	else
+	{
+		pid_controller::is_saturating_L = 0;
+	}
+	return pid_controller::u[0];
+}
+
+
+void pid_controller::integratorClamping(float error)
+{
+	/* if controller saturates and the error tends to saturate even more, then clamp integrator */
+	if((error > 0 && pid_controller::is_saturating_H) || (error < 0 && pid_controller::is_saturating_L))
+	{
+		pid_controller::clamping_flag = 1;
+		pid_controller::tmpKi = pid_controller::Ki;
+		pid_controller::Ki = 0;
+		pid_controller::update_parameters();
+	}
+	/* when stops saturating, or the error sign changes, unclamp integrator */
+	else
+	{
+		if(pid_controller::clamping_flag)
+		{
+			pid_controller::Ki = pid_controller::tmpKi;
+			pid_controller::update_parameters();
+			pid_controller::clamping_flag = 0;
+		}
+	}
+}
+
 /**
  * \addtogroup pid_getters PID setters
  * @{
@@ -377,9 +643,64 @@ void pid_controller::set_Kp(float Kp)
 	pid_controller::Kp = Kp;
 }
 
+void pid_controller::set_Kd_c(float Kd)
+{
+	pid_controller::Kd_c = Kd;
+}
+
+void pid_controller::set_Ki_c(float Ki)
+{
+	pid_controller::Ki_c = Ki;
+}
+
+void pid_controller::set_Kp_c(float Kp)
+{
+	pid_controller::Kp_c = Kp;
+}
+
+void pid_controller::set_Kd_r(float Kd)
+{
+	pid_controller::Kd_r = Kd;
+}
+
+void pid_controller::set_Ki_r(float Ki)
+{
+	pid_controller::Ki_r = Ki;
+}
+
+void pid_controller::set_Kp_r(float Kp)
+{
+	pid_controller::Kp_r = Kp;
+}
+
 void pid_controller::set_Ts(float Ts)
 {
 	pid_controller::Ts = Ts;
+}
+
+void pid_controller::set_wH(float wH)
+{
+	pid_controller::wH = wH;
+}
+
+void pid_controller::set_wL(float wL)
+{
+	pid_controller::wL = wL;
+}
+
+void pid_controller::set_N(float N)
+{
+	pid_controller::N = N;
+}
+
+void pid_controller::set_feedback_linearization(float feedback_linearization)
+{
+	pid_controller::feedback_linearization = feedback_linearization;
+}
+
+void pid_controller::set_command_vs_reject_thres(float command_vs_reject_thres)
+{
+	pid_controller::command_vs_reject_thres = command_vs_reject_thres;
 }
 /** @} */
 
@@ -402,9 +723,64 @@ float pid_controller::get_Kp()
 	return pid_controller::Kp;
 }
 
+float pid_controller::get_Kd_c()
+{
+	return pid_controller::Kd_c;
+}
+
+float pid_controller::get_Ki_c()
+{
+	return pid_controller::Ki_c;
+}
+
+float pid_controller::get_Kp_c()
+{
+	return pid_controller::Kp_c;
+}
+
+float pid_controller::get_Kd_r()
+{
+	return pid_controller::Kd_r;
+}
+
+float pid_controller::get_Ki_r()
+{
+	return pid_controller::Ki_r;
+}
+
+float pid_controller::get_Kp_r()
+{
+	return pid_controller::Kp_r;
+}
+
 float pid_controller::get_Ts()
 {
 	return pid_controller::Ts;
+}
+
+float pid_controller::get_N()
+{
+	return pid_controller::N;
+}
+
+float pid_controller::get_wH()
+{
+	return pid_controller::wH;
+}
+
+float pid_controller::get_wL()
+{
+	return pid_controller::wL;
+}
+
+float pid_controller::get_feedback_linearization()
+{
+	return pid_controller::feedback_linearization;
+}
+
+float pid_controller::get_command_vs_reject_thres()
+{
+	return pid_controller::command_vs_reject_thres;
 }
 /** @} */
 /** @} */
@@ -428,7 +804,7 @@ void linearTest(aeropendulum& aero)
 
 uint8_t caracterize(aeropendulum& aero)
 {
-	float motorPower = 2.2; 
+	float motorPower = 2.15; 
 	float encoderAngle = 0;
 	float tmpEncoderAngle;
 	aero.motorInit();
@@ -457,7 +833,7 @@ float feedbackLinearization(aeropendulum& aero)
 	float u0 = aero.get_propellerConst_u0();
 	float sinAngle = arm_sin_f32(aero.get_angle());
 	if(aero.get_angle() < PI/2) return u0 + sinAngle*MK;
-	else return u0 + sinAngle*MK - (aero.get_angle()-(PI/2))*aero.get_over90_compensation_cohef();
+	else return u0 + sinAngle*MK - (aero.get_angle()-0.85*(PI/2))*aero.get_over90_compensation_cohef();
 }
 /** @} */
 
@@ -484,41 +860,55 @@ void bangBangControl(aeropendulum& aero, float set_point)
 	}	
 }
 
-void pidControl(aeropendulum& aero ,pid_controller& piddy , float set_point)
+void pidControl(aeropendulum& aero ,pid_controller& pid , float set_point)
 {
-	arm_pid_instance_f32 pid;
+	//arm_pid_instance_f32 pid;
 	float feedbackTerm;
 	float pid_out;
 	float duty;
-
+	uint8_t is_command_mode;
+	uint8_t is_reject_mode;
 	aero.motorInit();
-	pid.Kp = piddy.get_Kp();
-	pid.Ki = piddy.get_Ki();
-	pid.Kd = piddy.get_Kd();
-	
-	arm_pid_init_f32(&pid, 1);
+
+	pid.init();
 
 	while(1)
 	{
 		aero.updateAngle();
+		if(aero.get_angle()-pid.get_command_vs_reject_thres()>set_point || aero.get_angle()+pid.get_command_vs_reject_thres()<set_point)
+		{
+			if(!is_command_mode)
+			{
+				pid.set_Kp(pid.get_Kp_c());	
+				pid.set_Ki(pid.get_Ki_c());	
+				pid.set_Kd(pid.get_Kd_c());
+				pid.update_parameters();	
+				is_command_mode = 1;
+			}
+		}
+		else
+		{
+			if(is_command_mode)
+			{
+				pid.set_Kp(pid.get_Kp_r());	
+				pid.set_Ki(pid.get_Ki_r());	
+				pid.set_Kd(pid.get_Kd_r());
+				pid.update_parameters();	
+				is_command_mode = 0;
+			}
+		}
 
-		pid_out = arm_pid_f32(&pid, set_point - aero.get_angle());
+		//pid_out = arm_pid_f32(&pid, set_point - aero.get_angle());
 		feedbackTerm = feedbackLinearization(aero);
+		pid.set_feedback_linearization(feedbackTerm);
+
+		pid_out = pid.update_manipulated(set_point - aero.get_angle());
+		
 
 		duty = pid_out + feedbackTerm;
 
-		/* Limit duty TODO: programm anti windup */
-		if(duty > 2.35)
-		{
-			duty = 2.35;
-		}
-		if(duty < 2.1)
-		{
-			duty = 2.1;
-		}
-		
 		aero.set_motorPower(duty);
-		vTaskDelay(msToTick(piddy.get_Ts()));
+		vTaskDelay(msToTick(pid.get_Ts()));
 	}
 }
 /** @} */
