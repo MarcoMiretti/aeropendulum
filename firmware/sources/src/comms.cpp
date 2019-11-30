16776b26cf6a5a1475a9b4b1f7f01418edc0dbd7
MarcoMiretti <marco.miretti@gmail.com>
2019-11-30 20:41:49 -0300

    :sparkles: Add UART5 configuration
    Add UART5 configuration to match HB01 (bluetooth dongle) baud rate. A
    single character is sent.

--- firmware/sources/src/comms.cpp
+++ firmware/sources/src/comms.cpp
@@ -29,6 +29,45 @@
 
 void aero_comms(void *pvParameters)
 {
+	/* Enable usart */
+	UART5->CR1 	|=	 (uint32_t)(1<<13);
+	/* 8 data bits */
+	UART5->CR1	&=	~(uint32_t)(1<<12);
+	/* 1 stop bit */
+	UART5->CR2	&=	~(uint32_t)(11<<12);
+	/* RXE interrupt inhibited */
+	UART5->CR1	&=	~(uint32_t)(1<<5);
+	/* TXE interrupt inhibited */
+	UART5->CR1	&=	~(uint32_t)(1<<7);
+	/* Parity control disable */
+	UART5->CR1	&=	~(uint32_t)(1<<10);
+	/* Oversampling 16 bits */
+	UART5->CR1	&=	~(uint32_t)(1<<15);
+	/* Half duplex mode not selected  */
+	UART5->CR3	&=	~(uint32_t)(1<<3);
+
+	/*
+	 * Baud Rate = fck/[8*(2-OVER8)*USARTDIV]
+	 * 
+	 * hence: USARTDIV = (fck/BR)/(8*(2-OVER8))
+	 * 
+	 * Since:
+	 * 	* fck = 16 MHz (HSI without prescallers)
+	 * 	* OVER8 = 0 (16 bits oversampling)
+	 * 	* BR = 9600 (depends on bluetooth dongle)
+	 *
+	 * Then:
+	 * 	* USARTDIV = 104.166666
+	 * 	Fraction: 0.1666*16=2.66==>3==>0x3
+	 * 	Mantissa: 104 ==> 0x68
+	 * */
+	UART5->BRR	=	(uint32_t)((0x68<<4)|(0x3));
+
 	while(1)
-	{}
+	{
+		UART5->CR1	|=  (uint32_t)(1<<3);
+		while(!(UART5->SR & USART_SR_TXE));
+		UART5->DR	 =  (uint8_t)('a');
+		vTaskDelay(msToTick(100));
+	}
 }
