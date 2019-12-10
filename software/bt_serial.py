8f2501af55711b0d8974e5c83134153e2e359edd
MarcoMiretti <marco.miretti@gmail.com>
2019-12-10 20:39:56 -0300

    :sparkles: Add caracterization from PC
    Add a python function that allows to caracterize the aeropendulum
    nonlinearities.

--- software/bt_serial.py
+++ software/bt_serial.py
@@ -1,7 +1,39 @@
 import time
 import serial
-import codecs
-import struct
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+writes in_str to bluetooth
+"""
+def bt_write( in_str ):
+    for i in range(32-len(in_str)):
+        in_str += '\0'
+    ser.write(in_str)
+    ser.flushOutput()
+
+"""
+reads variable value from bluetooth
+variable = string
+returns = float
+"""
+def bt_get( variable ):
+    rval = '0.0'
+    bt_write('p '+variable)
+    rval = ser.read(8)
+    rval = rval.replace('\x00','')
+    if rval == '':
+        rval = '0.0'
+    return float(rval)
+    
+"""
+sets a variable value through bluetooth
+variable = string
+value = float, int
+"""
+def bt_set( variable, value ):
+    bt_write('s '+variable+' '+str(float(value)))
+    print 's '+variable+' '+str(float(value))
 
 print 'port number:'
 input_port=raw_input()
@@ -11,9 +43,9 @@ ser = serial.Serial(
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
-
+    bytesize=serial.EIGHTBITS,
+    timeout=0.2
+    )
 ser.isOpen()
 
 print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
@@ -22,19 +54,61 @@ input=1
 while 1 :
     # get keyboard input
     input = raw_input(">> ")
-    for i in range(32-len(input)):
-        input += '\0'
     if input == 'exit':
         ser.close()
         exit()
-    else:
-        # send the character to the device
-        ser.write(input)
-        out = ''
-        # let's wait one second before reading output (let's give device time to answer)
+    if input == 'caracterize':
+        
+        motorPowers = []
+        angles = []
+        
+        plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        line1, = ax.plot(angles, motorPowers, 'b*')
+        plt.xlim(0.3, 1.6)
+        plt.ylim(2.15, 2.3)
+        
+        bt_set('mode',0)
+        print 'PC mode selected---'
         time.sleep(0.5)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
-
+        bt_set('onOff',1)
+        print 'Aero ON---'
+        time.sleep(2)
+        motorPower = 2.15
+        i = 0
+        last_angle = 0
+        while(last_angle<3.14159265/2):
+            new_angle = bt_get('angle')
+            while (new_angle < last_angle*0.95) or new_angle > last_angle*1.05:
+                last_angle = new_angle
+                print 'waiting for steady state'
+                time.sleep(1)
+            motorPowers.append(bt_get('motorPower'))
+            angles.append(new_angle)
+            motorPower += 0.001
+            bt_set('motorPower',motorPower)
+            last_angle = angles[-1]
+            i += 1
+            print 'Iteration '+str(i)
+            line1.set_xdata(angles)
+            line1.set_ydata(motorPowers)
+            plt.draw()
+            plt.pause(1e-17)
+            time.sleep(1)
+    if input == 'read':
+        out = ''
+        out += ser.read(ser.in_waiting)
         if out != '':
             print ">>" + out
+    if input[0] == 'p':
+        bt_write('p motorPower')
+        while ser.in_waiting < 8:
+            time.sleep(0.01)
+        out = ''
+        out += ser.read(ser.in_waiting)
+        ser.flushInput()
+        if out != '':
+            print '>>'+out
+    else:
+        bt_write(input)
